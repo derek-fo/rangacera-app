43240b087b3a309837c02078b875ac314676070f
Derek Oliveira <derek.foliveira@gmail.com>
2026-09-24 13:26:55 -0300

    chore: configura ESLint e Prettier

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ['dist/*'],
+  },
+  eslintConfigPrettier,
+]);
